69777d4160dc753d9918e80717e0549da27db6b3
yerrarahulsai <rahuldevops1802@gmail.com>
2025-09-24 16:21:56 +0530

    Declaring Variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Rahul
+PERSON2=Tony
+
+echo "$PERSON1: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine, What about you?"
+echo "$PERSON1: I am fine $PERSON2, what are you learning"
+echo "$PERSON2: I am learning shell scripting"
