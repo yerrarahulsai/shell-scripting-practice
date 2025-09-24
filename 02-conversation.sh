ff7d348194614d19105e01e1713b4b21482b184b
yerrarahulsai <rahuldevops1802@gmail.com>
2025-09-24 14:44:27 +0530

    Conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Rahul: Hi Tony, How are you?"
+echo "Tony: Hi Rahul, I am fine, What about you?"
+echo "Rahul: I am fine Tony, what are you learning"
+echo "Tony: I am learning shell scripting"
