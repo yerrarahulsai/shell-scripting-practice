0835e5309f5f3b85f71f4cb80180b261306c6eae
yerrarahulsai <rahuldevops1802@gmail.com>
2025-09-25 10:15:12 +0530

    Declaring Variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -7,3 +7,7 @@ echo "$PERSON1: Hi $PERSON2, How are you?"
 echo "$PERSON2: Hi $PERSON1, I am fine, What about you?"
 echo "$PERSON1: I am fine $PERSON2, what are you learning"
 echo "$PERSON2: I am learning shell scripting"
+
+#We can also refer like ${VAR_NAME}
+# So 2 ways 
+# $VAR_NAME or ${VAR_NAME}
