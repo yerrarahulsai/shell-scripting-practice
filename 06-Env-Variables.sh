8e522028fd80e61ea7d0b41e7c06bf51e45064d5
yerrarahulsai <rahuldevops1802@gmail.com>
2025-09-25 14:27:40 +0530

    Environment Variables

---
+++ 06-Env-Variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# We should set the environment variables in the linux cli
+# For only current session (Temporary)
+# We set it like - export COURSE="Shell Scriping"
+
+echo "Course is: $COURSE"
+
+#So if you want it premanently then we should add this variable in ~/.bashrc file
+# Here ~ represents home directory
+# Here in that file we will mention - export NAME="Shell"
+# Then we reload the configuration file by the command - source ~/.bashrc
+
+echo "Name is: $NAME"
