8f92a781c5b771a81d57e6eed691a2e291e0d46b
yerrarahulsai <rahuldevops1802@gmail.com>
2025-09-25 11:37:32 +0530

    Positional Parameters

---
+++ 04-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine, What about you?"
+echo "$PERSON1: I am fine $PERSON2, what are you learning"
+echo "$PERSON2: I am learning shell scripting"
+
+# sh 04-variables.sh Rahul Tony
+# We will pass the arguments to the script when we are running the script
+# $1 variable will hold the first argument passed
+# $2 variable will hold the second argument passed
+# These are called positional parameters
+# $1=Rahul, $2=Tony
