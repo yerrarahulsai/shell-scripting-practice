7163cf006b3e2a2a79eac95fab8a38d4b2bf5037
yerrarahulsai <rahuldevops1802@gmail.com>
2025-09-26 19:53:17 +0530

    Even-or-Odd

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -3,7 +3,7 @@
 # To determine wheather the number is even or odd
 read -p "Enter the number: " NUMBER
 
-if[ $(($NUMBER % 2)) -eq 0 ]; then
+if [ $(($NUMBER % 2)) -eq 0 ]; then
     echo "Given number is even"
 else
     echo "Given number is odd"
