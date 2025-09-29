65354fe084710cc03bcbc7413f3ecc76b8d71765
yerrarahulsai <rahuldevops1802@gmail.com>
2025-09-29 15:57:12 +0530

    Creating EC2 User through script

---
+++ 19-creating-ec2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Here we are creating instance and based on the input name we will print the IP address
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0a3dd7eb7d2bf30de"
+
+for instance in $@
+do 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi
+
+    echo "$instance: $IP"
+done
